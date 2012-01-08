f8814316f554cd88ed9e9717b245e5b03c35f213
Maxim Mai <maxim.sl.mai@gmail.com>
2012-01-08 00:06:00 -0500

    fixing the static path

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'zh-CN'
+LANGUAGE_CODE = 'en-US'
 
 SITE_ID = 1
 
@@ -83,6 +83,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'mysite.invoice',
     'django.contrib.admin',
+    'django.contrib.staticfiles',
 )
 
 LANGUAGE_FILES = {
