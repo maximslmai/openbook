f13b10ca0be1d8774f64e6f90fab8454aced5e1d
Maxim Mai <maxim.sl.mai@gmail.com>
2012-01-08 00:33:33 -0500

    fixing the static path

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/maximmai/webroot/openbook/media/'
+MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -78,7 +78,7 @@ TEMPLATE_DIRS = (
 
 STATIC_ROOT = "/home/maximmai/webroot/openbook/media/"
 
-STATIC_URL = "media/"
+STATIC_URL = "/static/media/"
 
 INSTALLED_APPS = (
     'django.contrib.auth',
